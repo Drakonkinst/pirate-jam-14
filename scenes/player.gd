c99378a30c1909edfc869afff744afb129a1843b
Drakonkinst <drakonkinstmc@gmail.com>
2024-01-14 18:08:43 -0800

    Switch project to jam prototype, add tilemap, collision, lighting

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 const WALK_RIGHT_ANIMATION = "walk"
 const WALK_UP_ANIMATION = "up"
@@ -13,19 +13,24 @@ var screen_size: Vector2 # Size of the game window
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	hide()
+	# hide()
 
 func _on_body_entered(_body: Node2D) -> void:
-	hide() # Player disappears after being hit.
-	hit.emit()
+	# hide() # Player disappears after being hit.
+	# hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
-	$PlayerHitbox.set_deferred("disabled", true)
+	# $PlayerHitbox.set_deferred("disabled", true)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity := _get_velocity()
-	set_pos(position + velocity * delta)
-	_update_animations(velocity)
+func _process(_delta: float) -> void:
+	_update_animations()
+	
+func _physics_process(_delta: float) -> void:
+	velocity = _get_velocity_from_input()
+	move_and_slide()
+	position = position.clamp(Vector2.ZERO, screen_size)
+
 
 # PUBLIC METHODS
 
@@ -34,30 +39,17 @@ func start(start_pos: Vector2) -> void:
 	show()
 	$PlayerHitbox.disabled = false
 	
-func set_pos(pos: Vector2) -> void:
-	position = pos.clamp(Vector2.ZERO, screen_size)
-	
 # HELPER METHODS (should all start with underscore)
 
-func _get_velocity() -> Vector2:
-	var horizontal_velocity := _get_velocity_multiplier("move_left", "move_right")
-	var vertical_velocity := _get_velocity_multiplier("move_up", "move_down")
-	var velocity_dir := Vector2(horizontal_velocity, vertical_velocity)	
+func _get_velocity_from_input() -> Vector2:
+	var velocity_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if velocity_dir.is_zero_approx():
 		return Vector2.ZERO
 	else:
 		return velocity_dir.normalized() * speed;
 
-func _get_velocity_multiplier(backward_action: String, forward_action: String) -> float:
-	var velocity: float = 0.0
-	if Input.is_action_pressed(backward_action):
-		velocity -= 1.0
-	if Input.is_action_pressed(forward_action):
-		velocity += 1.0
-	return velocity
-
 # Use scene unique node to refer to PlayerModel
-func _update_animations(velocity: Vector2) -> void:
+func _update_animations() -> void:
 	if velocity.is_zero_approx():
 		$PlayerModel.stop()
 	else:
