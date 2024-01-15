06b779a5587ddf96654e347be159c05b2f24817d
Drakonkinst <drakonkinstmc@gmail.com>
2024-01-15 12:06:59 -0800

    Remove demo scenes and assets

--- demo_scenes/mob.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody2D
-
-func _ready() -> void:
-	# Select a random model
-	var mob_types: PackedStringArray = $MobModel.sprite_frames.get_animation_names()
-	$MobModel.play(mob_types[randi() % mob_types.size()])
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
