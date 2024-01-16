c4ec19b2211adcdf1b3053fa4b5a1fd8b4d0104a
Drakonkinst <drakonkinstmc@gmail.com>
2024-01-15 21:16:52 -0800

    Modify screen resolution and scaling

--- scenes/camera_follow.gd
+++ scenes/camera_follow.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 class_name CameraFollow
 
+@export var enabled: bool = false
 @export var camera: Camera2D
 @export var left_bound: Marker2D
 @export var right_bound: Marker2D
 
 var y_pos: float
+
 func _ready() -> void:
 	y_pos = camera.position.y
 	
 func _process(_delta: float) -> void:
+	if not enabled:
+		return
 	# Always use original camera y position
 	#var camera_viewport = get_viewport().get_visible_rect().size
 	#print(global_position.x, " ", left_bound.position.x, " ", right_bound.position.x, " ", camera_viewport.x)
